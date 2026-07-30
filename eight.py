9effb6e60f4457e9fac589a440f905426f085071
pdishant005-cmyk <pdishant005@gmail.com>
2026-07-30 17:29:50 +0530

    Add files via upload

---
+++ eight.py
@@ -0,0 +1,12 @@
+
+name = "Python"
+print("Original String:", name)
+
+name = name + " Programming"
+print("Modified String:", name)
+
+numbers = [10, 20, 30]
+print("\nOriginal List:", numbers)
+
+numbers.append(40)
+print("Modified List:", numbers)
