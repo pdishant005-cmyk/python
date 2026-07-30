473f036c7dadda16a29f3c23fc93df38d4bba6ad
pdishant005-cmyk <pdishant005@gmail.com>
2026-07-30 16:47:40 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,15 @@
+# Basic Input and Output in Python
+print("Welcome to Python Programming!")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Name:", name)
+print("Age:", age)
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("Sum =", sum)
